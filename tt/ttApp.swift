5dd775f47651bb3fe23eed39418e3d2df4fa2337
nico <nicomenzii@gmail.com>
2024-12-09 12:44:13 +0100

    Initial Commit

---
+++ tt/ttApp.swift
@@ -0,0 +1,32 @@
+//
+//  ttApp.swift
+//  tt
+//
+//  Created by Nico Menzi on 09.12.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ttApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ ttTests/ttTests.swift
@@ -0,0 +1,17 @@
+//
+//  ttTests.swift
+//  ttTests
+//
+//  Created by Nico Menzi on 09.12.2024.
+//
+
+import Testing
+@testable import tt
+
+struct ttTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
